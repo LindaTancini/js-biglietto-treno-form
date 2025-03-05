b59d8aaca45ac61710125ee20673d217e64c82cb
LindaTancini <lindatancini95@gmail.com>
2025-03-05 16:50:52 +0100

    feat: added functional button

--- main.js
+++ main.js
@@ -15,33 +15,25 @@ const deleteButtonElement = document.getElementById("delete");
 console.log(deleteButtonElement);
 
 // rendo funzionanti i bottoni
-
 buttonElement.addEventListener("click", function () {
   console.log(nameElement.value);
   console.log(kmElement.value);
   console.log(ageElement.value);
+  // Età dell'utente
+  const userAge = ageElement.value;
+  // km da percorrere
+  const userKm = parseInt(kmElement.value);
+  // Dati delle variabili
+  const kmPrice = 0.21;
+  const basePrice = kmPrice * userKm;
+  let finalPrice = basePrice;
+  // Sconto 20% per minorenni < 18
+  if (userAge === "Minorenne") {
+    finalPrice *= 0.8;
+    // Sconto 40% over >65
+  } else if (userAge === "Over 65") {
+    finalPrice *= 0.6;
+  }
+  // Messaggio del prezzo finale
+  console.log(`Il prezzo del tuo biglietto è: ${finalPrice.toFixed(2)}€`);
 });
-
-// Età dell'utente
-const userAge = ageElement.value;
-console.log(userAge);
-
-// km da percorrere
-const userKm = parseInt(kmElement.value);
-console.log(userKm);
-
-// Dati delle variabili
-const kmPrice = 0.21;
-const basePrice = kmPrice * userKm;
-let finalPrice = basePrice;
-
-// Sconto 20% per minorenni < 18
-if (userAge === "Minorenne") {
-  finalPrice *= 0.8;
-  // Sconto 40% over >65
-} else if (userAge === "Over 65") {
-  finalPrice *= 0.6;
-}
-
-// Messaggio del prezzo finale
-console.log(`Il prezzo del tuo biglietto è: ${finalPrice.toFixed(2)}€`);
