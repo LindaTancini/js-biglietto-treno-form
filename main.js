db71a60ce2735df0aa58f8d6e2d879a3f3905421
LindaTancini <lindatancini95@gmail.com>
2025-03-05 16:40:50 +0100

    feat: add buttonelement with addeventlistener

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 console.log("Ciao Linda");
 
 // prendo gli elementi dalla pagina
+// nome, km e età
 const nameElement = document.getElementById("name");
 console.log(nameElement);
 const kmElement = document.getElementById("km");
 console.log(kmElement);
 const ageElement = document.getElementById("age");
 console.log(ageElement);
+//bottoni
+const buttonElement = document.getElementById("generate");
+console.log(buttonElement);
+const deleteButtonElement = document.getElementById("delete");
+console.log(deleteButtonElement);
+
+// rendo funzionanti i bottoni
+
+buttonElement.addEventListener("click", function () {
+  console.log(nameElement.value);
+  console.log(kmElement.value);
+  console.log(ageElement.value);
+});
 
 // Età dell'utente
 const userAge = ageElement.value;
